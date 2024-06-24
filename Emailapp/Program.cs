24793a7b79e374e51d3b45bf35736f017497ce1d
John van Dijk <fortyfour-three@teksavvy.com>
2024-06-23 21:11:21 -0400

    Add project files.

---
+++ Emailapp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Emailapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpClient smtpClient = new SmtpClient("mail.johnv.ca")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("john@johnv.ca", "AskSeekKnock//"),
+                EnableSsl = true,
+            };
+
+            MailMessage mailMessage = new MailMessage();
+ 
+            mailMessage.From = new MailAddress("john@johnv.ca");
+            mailMessage.To.Add("jvandijk.mail@gmail.com");
+
+            mailMessage.Subject = "The subject of my email";
+            mailMessage.Body = "<b>This is awesome</b>";
+            mailMessage.IsBodyHtml = true;
+
+            smtpClient.Send(mailMessage);
+            Console.WriteLine("Emailing Done!");
+        }
+    }
+}
